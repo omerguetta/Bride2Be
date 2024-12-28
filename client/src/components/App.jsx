ff43cebb6dbad4a7b3db6cd0ee9d7f6cbbdc13c3
omerguetta <omer.guetta@stu.shenkar.ac.il>
2024-12-28 20:19:24 +0200

    add router to App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+      </div>
+    </Router>
   );
 }
 
